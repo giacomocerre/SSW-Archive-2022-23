62e00f1b8e9b998a5e4d800a57b008ad683d9763
giacomocerre <cerre.giacomo93@gmail.com>
2023-05-21 12:04:50 +0200

    Add Button Component

--- src/app/components/atoms/button/button.component.ts
+++ src/app/components/atoms/button/button.component.ts
@@ -7,11 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   standalone: true
 })
 export class ButtonComponent {
-  classes: string = 'app-button'
   @Input() label: string;
   @Output() onClick: EventEmitter<void> = new EventEmitter<void>();
 
-  handleClick() {
-    this.onClick.emit();
-  }
 }
