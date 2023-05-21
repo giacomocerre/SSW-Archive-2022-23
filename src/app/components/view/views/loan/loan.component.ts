ce20feafa3e2934a637dfca18544431bfdc4e0c7
giacomocerre <cerre.giacomo93@gmail.com>
2023-05-21 22:34:32 +0200

    Updare Loan View Component

--- src/app/components/view/views/loan/loan.component.ts
+++ src/app/components/view/views/loan/loan.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { OpenerComponent } from 'src/app/components/atoms';
 
 @Component({
   selector: 'app-loan',
   templateUrl: './loan.component.html',
   styleUrls: ['./loan.component.scss'],
   standalone: true,
+  imports: [OpenerComponent]
 })
 export class LoanComponent {}
