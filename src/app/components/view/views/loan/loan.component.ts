d8862f75633eb02749535145ac08641bbf69925a
giacomocerre <cerre.giacomo93@gmail.com>
2023-05-21 12:07:55 +0200

    Add Loan View Component

---
+++ src/app/components/view/views/loan/loan.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loan',
+  templateUrl: './loan.component.html',
+  styleUrls: ['./loan.component.scss'],
+  standalone: true,
+})
+export class LoanComponent {}
