b1512d2e56d94efb12146c7b6c92ac0e1f43ec99
giacomocerre <cerre.giacomo93@gmail.com>
2023-05-21 12:05:33 +0200

    Add Book Component

--- src/app/components/molecules/book-card/book-card.component.ts
+++ src/app/components/molecules/book-card/book-card.component.ts
@@ -1,16 +1,21 @@
 import { Component, Input } from '@angular/core';
-import { SingleBookInterface } from '../../../types/interfaces';
-import { IconComponent } from '../../atoms';
 import { CommonModule } from '@angular/common';
-import { Book } from '../../../types/classes';
+import { IconComponent } from '../../atoms';
+import { OptionsMenuComponent } from '../options-menu/options-menu.component';
+import { Book } from '../../../models/classes';
 
 @Component({
   selector: 'app-book-card',
   templateUrl: './book-card.component.html',
   styleUrls: ['./book-card.component.scss'],
   standalone: true,
-  imports: [CommonModule,IconComponent],
+  imports: [CommonModule, IconComponent, OptionsMenuComponent],
 })
-export class BookCardComponent implements SingleBookInterface {
+export class BookCardComponent {
   @Input() item: Book;
+  showOptionsMenu:boolean = false;
+
+  handleOptionSelected(item: string) {
+    console.log(item)
+  }
 }
