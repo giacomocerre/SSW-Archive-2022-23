0abf6e462060565a942fc8f004368087e3240f42
giacomocerre <cerre.giacomo93@gmail.com>
2023-05-21 22:34:44 +0200

    Update Atoms and Molecules exports

--- src/app/components/atoms/index.ts
+++ src/app/components/atoms/index.ts
@@ -2,5 +2,12 @@ import { ButtonComponent } from './button/button.component';
 import { IconComponent } from './icon/icon.component';
 import { InputComponent } from './input/input.component';
 import { MessageComponent } from './message/message.component';
+import { OpenerComponent } from './opener/opener.component';
 
-export { IconComponent, ButtonComponent, MessageComponent, InputComponent };
+export {
+  IconComponent,
+  ButtonComponent,
+  MessageComponent,
+  InputComponent,
+  OpenerComponent,
+};
--- src/app/components/molecules/index.ts
+++ src/app/components/molecules/index.ts
@@ -1,13 +1,14 @@
+import { ArchiveViewerComponent } from './archive-viewer/archive-viewer.component';
 import { BookCardComponent } from './book-card/book-card.component';
 import { MenuComponent } from './menu/menu.component';
 import { OptionsMenuComponent } from './options-menu/options-menu.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { WidgetComponent } from './widget/widget.component';
-
 export {
   SidebarComponent,
   MenuComponent,
   WidgetComponent,
   BookCardComponent,
   OptionsMenuComponent,
+  ArchiveViewerComponent,
 };
