0a552179f7b9b2118993e3804c075f64ec722695
giacomocerre <cerre.giacomo93@gmail.com>
2023-05-22 15:28:55 +0200

    Add icon to input text

--- src/app/components/atoms/input/input.component.ts
+++ src/app/components/atoms/input/input.component.ts
@@ -1,11 +1,14 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IconComponent } from '../icon/icon.component';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
   standalone: true,
+  imports:[IconComponent]
 })
 export class InputComponent {
+  @Input() placeHolder: string = "input placeholder"
   @Output() onChange = new EventEmitter<string>();
 }
