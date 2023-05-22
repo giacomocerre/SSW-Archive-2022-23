93b4811945c2ee37d52d95ea72c8bba450ccf58c
giacomocerre <cerre.giacomo93@gmail.com>
2023-05-22 15:30:59 +0200

    Comment Archive Service

--- src/app/services/archive.service.ts
+++ src/app/services/archive.service.ts
@@ -9,17 +9,26 @@ import { Archive, Book } from '../models/classes';
 })
 export class ArchiveService {
   private baseUrl =
-    'https://eu-central-1.aws.data.mongodb-api.com/app/kvaas-giwjg/endpoint'; // KVaaS Endpoint
+    'https://eu-central-1.aws.data.mongodb-api.com/app/kvaas-giwjg/endpoint'; // Endpoint di KVaaS
   private apiKey = 'd6f7e1fe'; // Chiave API per l'autenticazione
-  private archiveSubject = new BehaviorSubject<Archive>(new Archive()); // BehaviorSubject per condividere l'istanza di Archive
-  public archive$: Observable<Archive> = this.archiveSubject.asObservable(); // Observable per sottoscriversi agli aggiornamenti dell'istanza di Archive
+
+  // BehaviorSubject per condividere l'istanza di Archive
+  private archiveSubject = new BehaviorSubject<Archive>(new Archive());
+
+  // Observable per sottoscriversi agli aggiornamenti dell'istanza di Archive
+  public archive$: Observable<Archive> = this.archiveSubject.asObservable();
 
   constructor(private http: HttpClient) {
+    // Recupera i dati iniziali dell'Archive dall'API e inizializza il BehaviorSubject
     this.fetchArchiveData().subscribe((archive) => {
-      this.archiveSubject.next(archive); // Inizializza il BehaviorSubject con l'istanza di Archive ottenuta dall'API
+      this.archiveSubject.next(archive);
     });
   }
 
+  /**
+   * Recupera i dati dell'Archive dall'API.
+   * @returns Un Observable che emette un'istanza di Archive.
+   */
   private fetchArchiveData(): Observable<Archive> {
     return this.http.get<string>(`${this.baseUrl}/get?key=${this.apiKey}`).pipe(
       map((response) => {
@@ -47,6 +56,11 @@ export class ArchiveService {
     );
   }
 
+  /**
+   * Aggiorna l'Archive sul server.
+   * @param body Il corpo della richiesta di aggiornamento.
+   * @returns Un Observable che emette la risposta dell'API.
+   */
   private updateArchive(body: Archive): Observable<any> {
     return this.http
       .post<any>(`${this.baseUrl}/set?key=${this.apiKey}`, JSON.stringify(body))
@@ -58,18 +72,29 @@ export class ArchiveService {
       );
   }
 
+  /**
+   * Aggiunge un libro all'Archive.
+   * @param book Il libro da aggiungere.
+   */
   addBookToArchive(book: Book): void {
     const archive = this.archiveSubject.getValue(); // Ottieni l'istanza corrente di Archive
     archive.addBook(book); // Aggiungi il libro all'istanza di Archive
     this.updateArchive(archive).subscribe();
   }
 
-  removeBookToArchive(id: string): void {
+  /**
+   * Rimuove un libro dall'Archive.
+   * @param id L'ID del libro da rimuovere.
+   */
+  removeBookFromArchive(id: string): void {
     const archive = this.archiveSubject.getValue(); // Ottieni l'istanza corrente di Archive
-    archive.removeBook(id); // rimovove il libro all'istanza di Archive
+    archive.removeBook(id); // Rimuove il libro dall'istanza di Archive
     this.updateArchive(archive).subscribe();
   }
 
+  /**
+   * Resetta Archive.
+   */
   resetArchive(): void {
     this.updateArchive(new Archive()).subscribe();
   }
