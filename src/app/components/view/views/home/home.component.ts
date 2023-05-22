580ace3dce864cc9a7d38a730b752e7703698d52
giacomocerre <cerre.giacomo93@gmail.com>
2023-05-22 15:30:48 +0200

    Update Home View logic

--- src/app/components/view/views/home/home.component.ts
+++ src/app/components/view/views/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MessageComponent, OpenerComponent } from '../../../../components/atoms';
-import { BookCardComponent, WidgetComponent } from '../../../../components/molecules';
+import { ArchiveViewerComponent, BookCardComponent, WidgetComponent } from '../../../../components/molecules';
 import { ArchiveService } from '../../../../services/archive.service';
 import { Book } from '../../../../models/classes';
 import { HttpClientModule } from '@angular/common/http';
@@ -11,7 +11,7 @@ import { HttpClientModule } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [HttpClientModule, CommonModule, OpenerComponent, WidgetComponent, BookCardComponent, MessageComponent],
+  imports: [HttpClientModule, CommonModule, OpenerComponent, WidgetComponent, MessageComponent, ArchiveViewerComponent],
   providers: [ArchiveService],
 })
 export class HomeComponent implements OnInit {
