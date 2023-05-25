dd1d3f47c9e5cef649296289406224279fe51432
giacomocerre <cerre.giacomo93@gmail.com>
2023-05-25 12:33:57 +0200

    Reset Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { ViewComponent } from './components/view/view.component';
-import { SidebarComponent } from './components/molecules';
-import { ArchiveComponent } from './components/view/views';
-import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [SidebarComponent, ViewComponent],
 })
 export class AppComponent {
-  view: string = 'home';
 }
--- src/app/components/view/views/index.ts
+++
@@ -1,5 +0,0 @@
-import { ArchiveComponent } from './archive/archive.component';
-import { HomeComponent } from './home/home.component';
-import { LoanComponent } from './loan/loan.component';
-
-export { HomeComponent, ArchiveComponent, LoanComponent };
