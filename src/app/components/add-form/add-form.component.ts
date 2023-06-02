d37b08b5a3edd274856cc80c49bb5a72e7493088
giacomocerre <cerre.giacomo93@gmail.com>
2023-06-02 13:02:09 +0200

    refactor add form component

--- src/app/components/add-form/add-form.component.ts
+++ src/app/components/add-form/add-form.component.ts
@@ -1,38 +1,31 @@
-import { HttpClientModule } from '@angular/common/http';
 import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { v4 as uuidv4 } from 'uuid';
 import { Book } from '../../models/classes';
 import { ArchiveService } from '../../services/archive.service';
-import { v4 as uuidv4 } from 'uuid';
 import { InputComponent } from '../input/input.component';
 import { ButtonComponent } from '../button/button.component';
-import { CommonModule } from '@angular/common';
 import { IconComponent } from '../icon/icon.component';
 import { MessageComponent } from '../message/message.component';
-import { ActionStatusInterface } from 'src/app/models/interfaces/classes.interfaces';
+import { ActionStatusInterface } from '../../models/interfaces/classes.interfaces';
 
 @Component({
   selector: 'app-add-form',
   templateUrl: './add-form.component.html',
   styleUrls: ['./add-form.component.scss'],
   standalone: true,
-  imports: [
-    CommonModule,
-    HttpClientModule,
-    InputComponent,
-    ButtonComponent,
-    IconComponent,
-    MessageComponent,
-  ],
+  imports: [ CommonModule, InputComponent, ButtonComponent, IconComponent, MessageComponent],
 })
 export class AddFormComponent {
   @Output() close = new EventEmitter<boolean>();
-  archivePosition: string;
-  title: string;
-  author: string;
+  archivePosition; title; author: string;
   message: ActionStatusInterface;
 
   constructor(private archiveService: ArchiveService) {}
 
+  /**
+   * Salva un nuovo libro nell'archivio.
+   */
   saveBook() {
     this.message = this.archiveService.addBookToArchive(
       new Book(
