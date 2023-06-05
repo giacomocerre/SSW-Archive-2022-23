efb7f4f68d490c14b988a0ded3b13acda70c8242
giacomocerre <cerre.giacomo93@gmail.com>
2023-06-05 10:17:11 +0200

    Remove uuid

--- src/app/components/add-form/add-form.component.ts
+++ src/app/components/add-form/add-form.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { v4 as uuidv4 } from 'uuid';
 import { Book } from '../../models/classes';
 import { ArchiveService } from '../../services/archive.service';
 import { InputComponent } from '../input/input.component';
@@ -14,11 +13,19 @@ import { ActionStatusInterface } from '../../models/interfaces/classes.interface
   templateUrl: './add-form.component.html',
   styleUrls: ['./add-form.component.scss'],
   standalone: true,
-  imports: [ CommonModule, InputComponent, ButtonComponent, IconComponent, MessageComponent],
+  imports: [
+    CommonModule,
+    InputComponent,
+    ButtonComponent,
+    IconComponent,
+    MessageComponent,
+  ],
 })
 export class AddFormComponent {
   @Output() close = new EventEmitter<boolean>();
-  archivePosition; title; author: string;
+  archivePosition;
+  title;
+  author: string;
   message: ActionStatusInterface;
 
   constructor(private archiveService: ArchiveService) {}
@@ -29,7 +36,7 @@ export class AddFormComponent {
   saveBook() {
     this.message = this.archiveService.addBookToArchive(
       new Book(
-        uuidv4(),
+        Math.random().toString(36).substring(7),
         this.archivePosition,
         this.title,
         this.author,
